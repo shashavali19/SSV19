ee1981862280aa5e58bc2549bced47933e277ac2
ssv <sheik.ssv1425@gmail.com>
2023-03-07 16:06:59 +0530

    project file

--- New.py
+++ New.py
@@ -1,8 +1,8 @@
-1 li = [1,2,3,4,5]
-2
-3 squares = [i**2 for i in li]
-4
-5 cube = [j**3 for j in li]
-6
-7 print ("the square of the mumber are",squares)
-8 print ("the cube of the mumber are",cube)
+ li = [1,2,3,4,5]
+
+ squares = [i**2 for i in li]
+
+ cube = [j**3 for j in li]
+
+ print ("the square of the mumber are",squares)
+ print ("the cube of the mumber are",cube)
